22bae70c813b41edaca18de36a1ab2f4c7a38592
rxpwang <wrxwst@gmail.com>
2024-06-26 22:22:43 -0400

    remove main print debug info

--- examples/main/main.cpp
+++ examples/main/main.cpp
@@ -1182,7 +1182,6 @@ int main(int argc, char ** argv) {
                 fprintf(stderr, "%s: failed to process audio\n", argv[0]);
                 return 10;
             }
-            fprintf(stderr, "audio information: pcmf32 size %d \n", pcmf32.size());
 
         }
 
