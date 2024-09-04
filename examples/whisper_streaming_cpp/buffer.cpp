771447dac7e11c12e6cfae6aca14f01d7a3de15d
Zhiming Xu <zhimng.xu@gmail.com>
2024-09-03 22:45:48 -0700

    feat: add hypothesis buffer implementation, need testing

---
+++ examples/whisper_streaming_cpp/buffer.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string>
+#include <tuple>
+#include <algorithm>
+#include <sstream>
+
+class HypothesisBuffer {
+    public:
+        std::vector<std::tuple<float, float, std::string>> self_committed_in_buffer;
+        std::vector<std::tuple<float, float, std::string>> self_buffer;
+        std::vector<std::tuple<float, float, std::string>> self_new;
+        float self_last_committed_time = 0;
+        std::string self_last_committed_word = std::string();
+
+        void insert(std::vector<std::tuple<float, float, std::string>>& new_, float offset) {
+            // add the offset to the new word list
+            std::transform(new_.begin(), new_.end(), new_.size(),
+                [offset](const std::tuple<float, float, std::string>& t) {
+                    return std::make_tuple(std::get<0>(t)+offset, std::get<1>(t)+offset, std::get<2>(t));
+                }
+            );
+
+            // update this->new_ if the word timestamp is in or after the last 0.1s of the last committed time
+            std::copy_if(new_.begin(), new_.end(), std::back_inserter(self_new),
+                [this](const std::tuple<float, float, std::string>& t) {
+                    return std::get<0>(t) > self_last_committed_time - 0.1;
+                }
+            );
+
+            // the loop
+            if (!self_new.empty()) {
+                float a, b;
+                std::string t;
+                std::tie(a, b, t) = self_new[0];
+                if (std::abs(a - self_last_committed_time) < 1) {
+                    if (!self_committed_in_buffer.empty()) {
+                        size_t cn = self_committed_in_buffer.size();
+                        size_t nn = self_new.size();
+
+                        for (int i=1; i<=std::min((int)std::min(cn, nn), 5); i++) {
+                            // get c
+                            std::vector<std::string> extracted;
+                            for (size_t j=i; j>=1; j--) {
+                                extracted.push_back(
+                                    std::get<2>(self_committed_in_buffer[self_committed_in_buffer.size()-j])
+                                );
+                            }
+
+                            std::ostringstream oss;
+                            for (size_t j=0; j<extracted.size(); j++) {
+                                if (j!=0) {
+                                    oss << " ";
+                                }
+                                oss << extracted[j];
+                            }
+
+                            std::string c = oss.str();
+
+                            // get tail
+                            extracted.clear();
+                            oss.clear();
+                            for (size_t j=1; j<=i; j++) {
+                                if (j!=1) {
+                                    oss << " ";
+                                }
+                                oss << std::get<2>(self_new[j-1]);
+                            }
+
+                            std::string tail = oss.str();
+
+                            // compare and remove
+                            if (c == tail) {
+                                std::vector<std::string> words = {};
+                                for (size_t j=0; j<i; j++) {
+                                    std::stringstream ss;
+                                    std::tie(a, b, t) = self_new[0];
+                                    self_new.erase(self_new.begin());
+                                    ss << a << " " << b << " " << t;
+                                    words.push_back(ss.str());
+                                }
+
+                                oss.clear();
+                                for (size_t j=0; j<=words.size(); j++) {
+                                    if (j!=0) {
+                                        oss << " ";
+                                    }
+                                    oss << words[j];
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        std::vector<std::tuple<float, float, std::string>> flush() {
+            std::vector<std::tuple<float, float, std::string>> commit = {};
+            float na, nb;
+            std::string nt;
+            while (!self_new.empty()) {
+                std::tie(na, nb, nt) = self_new[0];
+                if (self_buffer.empty()) {
+                    break;
+                }
+
+                if (nt == std::get<2>(self_buffer[0])) {
+                    commit.emplace_back(na, nb, nt);
+                    self_last_committed_word = nt;
+                    self_last_committed_time = nb;
+                    self_buffer.erase(self_buffer.begin());
+                    self_new.erase(self_new.begin());
+                } else {
+                    break;
+                }
+            }
+
+            self_buffer = self_new;
+            self_new.clear();
+            self_committed_in_buffer.insert(self_committed_in_buffer.end(), commit.begin(), commit.end());
+            return commit;
+        }
+
+        void pop_committed(float t) {
+            while (!self_committed_in_buffer.empty() &&
+                std::get<1>(self_committed_in_buffer[0]) <= t) {
+                self_committed_in_buffer.erase(self_committed_in_buffer.begin());
+            }
+        }
+
+        std::vector<std::tuple<float, float, std::string>> complete() {
+            return self_buffer;
+        }
+
+};
