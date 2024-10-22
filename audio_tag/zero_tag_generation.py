fc3f577f61f5b43a4f9e809fc2af83a91122046d
rxpwang <waq9hw@virginia.edu>
2024-10-21 22:11:59 -0400

    chore: update zero mixed tag script.

--- audio_tag/zero_tag_generation.py
+++ audio_tag/zero_tag_generation.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-zero_tag = np.zeros(64000)
-zero_tag_df = pd.DataFrame(zero_tag)
-zero_tag_df.to_csv("zero_tag_4.csv", index=False, header=False)
-print("Zero tag saved to zero_tag_4.csv")
+for i in [0.5, 1]:
+    zero_tag =  np.zeros(int(i * 16000)) #np.random.normal(0, 1, int(i * 16000))
+    zero_tag_df = pd.DataFrame(zero_tag)
+    zero_tag_df.to_csv("zero_tag_"+str(i)+".csv", index=False, header=False)
+    #print("Zero tag saved to wn_tag_4.csv")
