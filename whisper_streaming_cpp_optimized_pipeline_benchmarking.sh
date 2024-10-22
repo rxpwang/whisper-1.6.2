a1327e4a8f7c95be125bf770c212291629e4e9e1
rxpwang <waq9hw@virginia.edu>
2024-10-21 21:16:07 -0400

    chore: update benchmarking script for token latency.

--- whisper_streaming_cpp_optimized_pipeline_benchmarking.sh
+++ whisper_streaming_cpp_optimized_pipeline_benchmarking.sh
@@ -7,15 +7,17 @@ dtw="medium"
 threads="12"
 
 # Array of different step lengths to loop through
-step_lengths=(300 500 800 1000 2000)
+step_lengths=(300 400 500)
 
 # Loop over each step length
 for step in "${step_lengths[@]}"; do
     # Create the log file name based on the parameters, including the current step length
-    log_file="benchmarking_results/whisper_streaming_cpp_optimized_pipeline_${model}_${sample}_dtw-${dtw}_step-${step}_t-${threads}.log"
+    log_file="benchmarking_results/whisper_streaming_cpp_optimized_pipeline_${model}_${sample}_dtw-${dtw}_step-${step}_t-${threads}_with_token_latency.log"
+    log_file_no_audio_tag="benchmarking_results/whisper_streaming_cpp_optimized_pipeline_${model}_${sample}_dtw-${dtw}_step-${step}_t-${threads}_with_token_latency_no_audio_tag.log"
     
     # Run the command and redirect the output to the log file
     ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw -ac -1 -at audio_tag/medium_0.5s_avg.csv --step $step -t $threads > $log_file 2>&1
+    ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw --step $step -t $threads > $log_file_no_audio_tag 2>&1
     
     echo "Output has been logged to $log_file"
 done
