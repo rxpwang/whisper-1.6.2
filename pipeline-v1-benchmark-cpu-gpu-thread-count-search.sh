05983084cee375fce54c71185e4fba826d12c8db
Zhiming Xu <zhimng.xu@gmail.com>
2024-11-02 15:41:27 -0700

    chore: update benchmark script

--- pipeline-v1-benchmark-cpu-gpu-thread-count-search.sh
+++ pipeline-v1-benchmark-cpu-gpu-thread-count-search.sh
@@ -6,21 +6,28 @@ if [[ "$(pwd)" != *"whisper"* ]]; then
 fi
 
 # Parameters from your command
-model_type="base"
-model="ggml-${model_type}.bin"
+model_type="medium"
+repetitions=5
+
+if [ $# == 1 ]; then
+    model_type=$1
+elif [ $# == 2 ]; then
+    model_type=$1
+    repetitions=$2
+fi
+
 sample="bernie4min.wav"
 dtw=${model_type}
 
 # Get the number of all CPU cores on macOS, use all but 2 cores
-total_threads=$(($(sysctl -n hw.physicalcpu)-2))
+total_threads=$(($(sysctl -n hw.physicalcpu)-1))
 # Fallback to a default value if the command fails
 if [ -z "$threads" ]; then
     threads="12"
 fi
 
 # Array of different step lengths to loop through
-runs=5
-step=100
+step=10
 max_decoder=5
 base_directory="benchmarking_results"
 exp_name="thread_count_search"
@@ -29,15 +36,15 @@ result_dir="${base_directory}/${exp_name}/${model_type}"
 mkdir -p $result_dir
 for cpu_threads in $(seq $max_decoder $(($total_threads-1))); do
     # Run each config for `runs` times
-    for i in $(seq 1 $runs); do
+    for i in $(seq 1 $repetitions); do
         gpu_threads=$(($total_threads - $cpu_threads))
         echo "Run ${i} cpu threads: ${cpu_threads}, gpu threads: ${gpu_threads}"
         # Create the log file name based on the parameters, including the current step length
-        log_file="pipeline-v1_${model}_${sample}_dtw-${dtw}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_with_audio_tag_hallucination_fixed_run-${i}.log"
-        log_file_no_audio_tag="pipeline-v1_${model}_${sample}_dtw-${dtw}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_run-${i}.log"
+        log_file="pipeline-v1_${model_type}_${sample}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_with_tag_run-${i}.log"
+        log_file_no_audio_tag="pipeline-v1_${model}_${sample}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_no_tag_run-${i}.log"
         
         # Run the command and redirect the output to the log file
-        ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw -ac -1 -at audio_tag/base_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads > $result_dir/$log_file 2>&1
+        ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw -ac -1 -at audio_tag/${model_type}_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads > $result_dir/$log_file 2>&1
         echo "Output has been logged to ${result_dir}/${log_file}"
 
         echo "Waiting for 30 seconds before the next run..."
@@ -46,14 +53,16 @@ for cpu_threads in $(seq $max_decoder $(($total_threads-1))); do
         ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw --step $step -ct $cpu_threads -gt $gpu_threads > $result_dir/$log_file_no_audio_tag 2>&1
         echo "Output has been logged to ${result_dir}/${log_file_no_audio_tag}"
 
-        echo "Waiting for 60 seconds before the next run..."
-        sleep 60
+        if [ "$i" -lt "$repetitions" ]; then
+            echo "Waiting for 30 seconds before the next run..."
+            sleep 30
+        fi
     done
 done
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    system_profiler SPHardwareDataType > benchmarking_results/00-systeminfo.txt
+    system_profiler SPHardwareDataType > ${result_dir}/00-systeminfo.txt
 fi
 # Append the current git commit hash to the system info file
-echo "Current Git Commit:" >> benchmarking_results/00-systeminfo.txt
-git rev-parse HEAD >> benchmarking_results/00-systeminfo.txt
+echo "Current Git Commit:" >> ${result_dir}/00-systeminfo.txt
+git rev-parse HEAD >> ${result_dir}/00-systeminfo.txt
--- pipeline-v1-benchmark-max-decode-round-search.sh
+++ pipeline-v1-benchmark-max-decode-round-search.sh
@@ -6,7 +6,16 @@ if [[ "$(pwd)" != *"whisper"* ]]; then
 fi
 
 # Parameters from your command
-model_type="base"
+model_type="medium"
+repetitions=5
+
+if [ $# == 1 ]; then
+    model_type=$1
+elif [ $# == 2 ]; then
+    model_type=$1
+    repetitions=$2
+fi
+
 model="ggml-${model_type}.bin"
 sample="bernie4min.wav"
 dtw=${model_type}
@@ -19,7 +28,6 @@ if [ -z "$threads" ]; then
 fi
 
 # Array of different step lengths to loop through
-runs=5
 step=10
 cpu_threads=5 # use at least 5 threads for CPU functions
 gpu_threads=$(($total_threads-$cpu_threads))
@@ -38,14 +46,14 @@ mkdir -p $result_dir
 max_decoding_round_factor=(0.25 0.33 0.5 0.67 0.75)
 for max_decoding_round in "${max_decoding_round_factor[@]}"; do
     # Run each config for `runs` times
-    for i in $(seq 1 $runs); do
+    for i in $(seq 1 $repetitions); do
         echo "Run ${i} cpu threads: ${cpu_threads}, gpu threads: ${gpu_threads}, max decoding round: ${max_decoding_round}"
         # Create the log file name based on the parameters, including the current step length
-        log_file="pipeline-v1_${model}_${sample}_dtw-${dtw}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_mdr-${max_decoding_round}_with_tag_run-${i}.log"
-        log_file_no_audio_tag="pipeline-v1_${model}_${sample}_dtw-${dtw}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_mdr-${max_decoding_round}_no_tag_run-${i}.log"
+        log_file="pipeline-v1_${model_type}_${sample}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_mdr-${max_decoding_round}_with_tag_run-${i}.log"
+        log_file_no_audio_tag="pipeline-v1_${model_type}_${sample}_step-${step}_ct-${cpu_threads}_gt-${gpu_threads}_mdr-${max_decoding_round}_no_tag_run-${i}.log"
  
         # Run the command and redirect the output to the log file
-        ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw -ac -1 -at audio_tag/base_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads -mdr $max_decoding_round > $result_dir/$log_file 2>&1
+        ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw -ac -1 -at audio_tag/${model_type}_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads -mdr $max_decoding_round > $result_dir/$log_file 2>&1
         echo "Output has been logged to ${result_dir}/${log_file}"
 
         echo "Waiting for 30 seconds before the next run..." 
@@ -54,14 +62,16 @@ for max_decoding_round in "${max_decoding_round_factor[@]}"; do
         ./whisper_streaming_cpp_optimized -m models/$model samples/$sample -kc -dtw $dtw --step $step -ct $cpu_threads -gt $gpu_threads -mdr $max_decoding_round > $result_dir/$log_file_no_audio_tag 2>&1
         echo "Output has been logged to ${result_dir}/${log_file_no_audio_tag}"
 
-        echo "Waiting for 60 seconds before the next run..."
-        sleep 60
+        if [ "$i" -lt "$repetitions" ]; then
+            echo "Waiting for 30 seconds before the next run..."
+            sleep 30
+        fi
     done
 done
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    system_profiler SPHardwareDataType > benchmarking_results/00-systeminfo.txt
+    system_profiler SPHardwareDataType > ${result_dir}/00-systeminfo.txt
 fi
 # Append the current git commit hash to the system info file
-echo "Current Git Commit:" >> benchmarking_results/00-systeminfo.txt
-git rev-parse HEAD >> benchmarking_results/00-systeminfo.txt
+echo "Current Git Commit:" >> ${result_dir}/00-systeminfo.txt
+git rev-parse HEAD >> ${result_dir}/00-systeminfo.txt
