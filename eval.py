b66f5cf4d694ce94e9038a810119c0fe481b27a8
Zhiming Xu <zhimng.xu@gmail.com>
2024-11-10 21:03:51 -0800

    chore: branch for baseline

--- eval.py
+++ eval.py
@@ -20,10 +20,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     print(f"Evaluating system {args.system} with model {args.model_type} on dataset {args.dataset}")
-    if args.system == "whisperflow":
-        program = "./whisper_streaming_cpp_optimized"
-    elif args.system == "baseline":
-        program = "./whisper_streaming_cpp"
  
     if args.thread == -1:
         print(f"Using {args.cpu_thread} CPU threads and {args.gpu_thread} GPU threads")
@@ -41,21 +37,37 @@ if __name__ == '__main__':
         if path.endswith(".wav"):
             data_path = os.path.join(data_dir, path)
             result_path = os.path.join(result_dir, f"{os.path.splitext(os.path.basename(path))[0]}.log")
-            arg_list = [
-                # ./whisper_streaming_cpp_optimized -m models/$model $data_dir/$sample -kc -dtw $dtw -ac -1 -at audio_tag/${model_type}_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads -mdr $mdr >> $log_file 2>&1
-                program,
-                "-m", f"models/ggml-{args.model_type}.bin",
-                data_path,
-                "-kc",
-                "-dtw", args.model_type,
-                "-ac", "-1",
-                "-at", f"audio_tag/{args.model_type}_0.5s_avg.csv",
-                "--step", f"{args.step}",
-                "-ct", f"{args.cpu_thread}",
-                "-gt", f"{args.gpu_thread}",
-                "-mdr", f"{args.max_decoding_round}",
-                ">>", result_path,
-                "2>&1",
-            ]
+            if args.system == "whisperflow":
+                program = "./whisper_streaming_cpp_optimized"
+                arg_list = [
+                    # ./whisper_streaming_cpp_optimized -m models/$model $data_dir/$sample -kc -dtw $dtw -ac -1 -at audio_tag/${model_type}_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads -mdr $mdr >> $log_file 2>&1
+                    program,
+                    "-m", f"models/ggml-{args.model_type}.bin",
+                    data_path,
+                    "-kc",
+                    "-dtw", args.model_type,
+                    "-ac", "-1",
+                    "-at", f"audio_tag/{args.model_type}_0.5s_avg.csv",
+                    "--step", f"{args.step}",
+                    "-ct", f"{args.cpu_thread}",
+                    "-gt", f"{args.gpu_thread}",
+                    "-mdr", f"{args.max_decoding_round}",
+                    ">>", result_path,
+                    "2>&1",
+                ]
+            elif args.system == "baseline":
+                program = "./whisper_streaming_cpp"
+                arg_list = [
+                    # ./whisper_streaming_cpp_optimized -m models/$model $data_dir/$sample -kc -dtw $dtw -ac -1 -at audio_tag/${model_type}_0.5s_avg.csv --step $step -ct $cpu_threads -gt $gpu_threads -mdr $mdr >> $log_file 2>&1
+                    program,
+                    "-m", f"models/ggml-{args.model_type}.bin",
+                    data_path,
+                    "-kc",
+                    "-dtw", args.model_type,
+                    "--step", f"{args.step}",
+                    "-t", f"{args.thread}",
+                    ">>", result_path,
+                    "2>&1",
+                ]
             subprocess.run(" ".join(arg_list), shell=True)
  
