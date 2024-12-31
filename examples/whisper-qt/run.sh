4b7ba15499e012b9faf4e9a462e9dea5b0d74ee9
rxpwang <wrxwst@gmail.com>
2024-12-31 00:11:17 -0500

    fix: quick fix for metal backend detection. may come up with better approaches later.

--- examples/whisper-qt/run.sh
+++ examples/whisper-qt/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir whisper-qt.app/Contents/Resources
+cp ../../ggml-metal.metal whisper-qt.app/Contents/Resources/
+cp ../../ggml-common.h whisper-qt.app/Contents/Resources/
 whisper-qt.app/Contents/MacOS/whisper-qt    \
     -m ../../models/ggml-small.bin \
     samples/bernie30s.wav \
