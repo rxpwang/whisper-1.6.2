0ca865597f9f9a678acfdf2a226264f783245599
rxpwang <wrxwst@gmail.com>
2025-01-07 12:51:39 -0500

    fix: step length set short to put system in the best effort mode.

--- examples/whisper-qt/run.sh
+++ examples/whisper-qt/run.sh
@@ -6,4 +6,4 @@ whisper-qt.app/Contents/MacOS/whisper-qt    \
     -m ../../models/ggml-small.bin \
     samples/bernie30s.wav \
     -kc -dtw small -ac -1 \
-    -at ../../audio_tag/small_0.5s_avg.csv
+    -at ../../audio_tag/small_0.5s_avg.csv --step 100
